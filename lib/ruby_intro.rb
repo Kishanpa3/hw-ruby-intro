6009f7e427e506b4d2472b7beafe44a3822b8ee7
Ubuntu <ubuntu@ip-172-31-81-22.ec2.internal>
2019-09-15 02:26:25 +0000

    [Added] - Hw1 Solution

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,89 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum()
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.sort().reverse().take(2).sum()
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.permutation(2).to_a.each do |pair|
+    if pair.sum == n then
+      return true
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length == 0 then
+    return false
+  end
+  if not s[0].upcase =~ /[A-Z]/ then
+    return false
+  end
+  if s[0].upcase =~ /[AEIOU]/ then
+    return false
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0 then
+    return false
+  end
+  
+  s.chars.each do |bit|
+    if bit == '1' or bit == '0' then
+    else
+      return false
+    end
+  end
+  
+  
+  return s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.length == 0 then
+      raise ArgumentError
+    end
+    if price <= 0 then
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn=(num)
+    @isbn = num
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price=(num)
+    @price = num
+  end
+  
+  def price
+    @price
+  end
+  
+  def price_as_string
+    return "$%0.2f" % [price]
+  end
+  
 end
